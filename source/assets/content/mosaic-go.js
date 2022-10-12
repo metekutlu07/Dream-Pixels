0fcd480d28f812f0ea3fb2a9db6edf856e8d0ce0
mete kutlu <metekutlu@gmail.com>
2022-10-13 00:28:01 +0200

    mosaic go content update (tessera)

--- source/assets/content/mosaic-go.js
+++ source/assets/content/mosaic-go.js
@@ -1,15 +1,133 @@
 export default {
 
 	path: 'mosaic-go',
-	title: 'Mosaic Go !',
+	title: 'Tessera',
 	subtitle: 'Roman Mosaic in AR',
-	location: 'Paris, France',
-	date: '2020',
-	team: 'Mete Kutlu & Theuns Botha',
-	period: 'June 2021 - October 2022',
-	partners: 'Comet Lab & Ausser',
-	clients: 'Foundation Philippe Auguste',
-	description: '',
-	sections: []
+	location: 'Carthage',
+	date: 'July 2019',
+	team: 'Mete Kutlu',
+	period: 'July 2019',
+	description: 'In this experiment, I made a 3D model of an Ottoman miniature representing the city of Constantinople. This virtual three-dimensional miniature allows us to go through the golden frame and the pictorial surface of pigments, and finally dive into the city from different angles. We can walk through its alleys, fly around its domes and explore the hidden corners of Constantinople as imagined by the miniaturist.',
+	sections: [
+
+		{
+			type: 'S1',
+			media: {
+				source: 'public/mosaic-go/main.jpg',
+				caption: 'Video Presentation',
+				tags: [ 'Artificial Intelligence', 'Persian Miniature' ],
+			}
+		},
+
+		{
+			type: 'S3',
+			media: {
+				source: 'public/mosaic-go/main.jpg',
+				caption: 'Video Presentation',
+				tags: [ 'Artificial Intelligence', 'Persian Miniature' ],
+			}
+		},
+
+		{
+			type: 'S3',
+			media: {
+				source: 'public/mosaic-go/echelle.jpg',
+				caption: 'Video Presentation',
+				tags: [ 'Artificial Intelligence', 'Persian Miniature' ],
+			}
+		},
+
+
+
+		{
+			type: 'S3',
+			media: {
+				source: 'public/mosaic-go/carthage.jpg',
+				caption: 'Video Presentation',
+				tags: [ 'Artificial Intelligence', 'Persian Miniature' ],
+			}
+		},
+
+		{
+			type: 'S3',
+			media: {
+				source: 'public/mosaic-go/parc.jpg',
+				caption: 'Video Presentation',
+				tags: [ 'Artificial Intelligence', 'Persian Miniature' ],
+			}
+		},
+
+
+		{
+			type: 'S3',
+			media: {
+				source: 'public/mosaic-go/maison.jpg',
+				caption: 'Video Presentation',
+				tags: [ 'Artificial Intelligence', 'Persian Miniature' ],
+			}
+		},
+
+		{
+			type: 'S3',
+			media: {
+				source: 'public/mosaic-go/map.jpg',
+				caption: 'Video Presentation',
+				tags: [ 'Artificial Intelligence', 'Persian Miniature' ],
+			}
+		},
+
+
+		{
+			type: 'S3',
+			media: {
+				source: 'public/mosaic-go/mosaic-ar.jpg',
+				caption: 'Video Presentation',
+				tags: [ 'Artificial Intelligence', 'Persian Miniature' ],
+			}
+		},
+
+		{
+			type: 'S3',
+			media: {
+				source: 'public/mosaic-go/triclin01.jpg',
+				caption: 'Video Presentation',
+				tags: [ 'Artificial Intelligence', 'Persian Miniature' ],
+			}
+		},
+
+		{
+			type: 'S3',
+			media: {
+				source: 'public/mosaic-go/triclin02.jpg',
+				caption: 'Video Presentation',
+				tags: [ 'Artificial Intelligence', 'Persian Miniature' ],
+			}
+		},
+
+
+		{
+			type: 'S3',
+			media: {
+				source: 'public/mosaic-go/cheveaux.jpg',
+				caption: 'Video Presentation',
+				tags: [ 'Artificial Intelligence', 'Persian Miniature' ],
+			}
+		},
+
+		{
+			type: 'S3',
+			media: {
+				source: 'public/mosaic-go/menu.jpg',
+				caption: 'Video Presentation',
+				tags: [ 'Artificial Intelligence', 'Persian Miniature' ],
+			}
+		},
+
+
+
+
+
+
+	]
 
 };
