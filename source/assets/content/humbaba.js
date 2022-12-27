b9a464a3d90d01510eb4ac18a1527575d6a821fd
mete kutlu <metekutlu@gmail.com>
2022-12-27 01:10:11 +0100

    Update Humbaba

--- source/assets/content/humbaba.js
+++ source/assets/content/humbaba.js
@@ -14,109 +14,197 @@ export default {
 		{
 			type: 'S1',
 			media: {
-				source: 'public/humbaba/002.mp4',
+				source: 'public/humbaba/t01.mp4',
 				caption: 'Video Introduction'
 			}
 		},
+
+		{
+			type: 'S2',
+			title: 'Animations',
+		},
+
+		{
+			type: 'S3',
+			anchor: 'Animations',
+			media: {
+				source: 'public/humbaba/disco.mp4',
+				tags: [ 'Projection Mapping', 'Sumerian Mythology', 'Motion Graphics' ],
+				caption: 'Disco Lion'
+			}
+		},
+
 		{
 			type: 'S3',
 			media: {
-				source: 'public/humbaba/humbaba-histogram.mp4',
-				tags: [ 'Korean Architecture' ],
-				caption: 'Humbaba Caption'
+				source: 'public/humbaba/edgez.mp4',
+				tags: [ 'Projection Mapping', 'Sumerian Mythology', 'Motion Graphics' ],
+				caption: 'Shadows'
 			}
 		},
+
 		{
 			type: 'S3',
+			media: {
+				source: 'public/humbaba/green.mp4',
+				tags: [ 'Projection Mapping', 'Sumerian Mythology', 'Motion Graphics' ],
+				caption: 'Laser Lion'
+			}
+		},
+
+		{
+			type: 'S3',
+			media: {
+				source: 'public/humbaba/img/videos.jpg',
+				tags: [ 'Projection Mapping', 'Sumerian Mythology', 'Motion Graphics' ],
+				caption: 'Humbaba Experiments'
+			}
+		},
+
+		{
+			type: 'S2',
+			title: 'Storyboard',
+		},
+
+		{
+			type: 'S3',
+			media: {
+				excludeFromSphere: true,
+
+				source: 'public/humbaba/img/test3.png',
+				tags: [ 'Projection Mapping', 'Sumerian Mythology', 'Motion Graphics' ],
+
+				caption: 'A Name Eternal'
+			}
+		},
+
+		{
+			type: 'S3',
+			anchor: 'Storyboard',
+			media: {
+				excludeFromSphere: true,
+
+				source: 'public/humbaba/img/test2.png',
+				tags: [ 'Projection Mapping', 'Sumerian Mythology', 'Motion Graphics' ],
+
+				caption: 'An Unwinnable Battle'
+			}
+		},
+
+		{
+			type: 'S3',
+			media: {
+				excludeFromSphere: true,
+
+				source: 'public/humbaba/img/test4.png',
+				tags: [ 'Projection Mapping', 'Sumerian Mythology', 'Motion Graphics' ],
+
+				caption: 'Death of Humbaba'
+			}
+		},
+
+		{
+			type: 'S2',
+			title: 'Projection Mapping',
+		},
+
+		{
+			type: 'S3',
+			anchor: 'Projection Mapping',
 			media: {
 				source: 'public/humbaba/humbaba1.mp4',
-				tags: [ 'Photogrammetry' ],
-				caption: 'Humbaba Caption'
+				tags: [ 'Projection Mapping', 'Sumerian Mythology', 'Motion Graphics' ],
+				caption: 'Chapter 1'
 			}
 		},
+
+
 		{
-			type: 'S3',
-			media: {
-				source: 'public/humbaba/img/videos.png',
-				tags: [ 'Photogrammetry' ],
-				caption: 'Humbaba Caption'
-			}
-		},
-		{
-			anchor: 'Istanbul University Library',
 			type: 'S3',
 			media: {
 				source: 'public/humbaba/humbaba2.mp4',
-				tags: [ 'Photogrammetry' ],
-				caption: 'Humbaba Caption'
+				tags: [ 'Projection Mapping', 'Sumerian Mythology', 'Motion Graphics' ],
+				caption: 'Chapter 2'
 			}
 		},
-		{
-			type: 'S3',
-			media: {
-				source: 'public/humbaba/img/madmapper1.png',
-				tags: [ 'Photogrammetry' ],
-				caption: 'Humbaba Caption'
-			}
-		},
+
 		{
 			type: 'S3',
 			media: {
 				source: 'public/humbaba/humbaba3.mp4',
-				tags: [ 'Photogrammetry' ],
-				caption: 'Humbaba Caption'
+				tags: [ 'Projection Mapping', 'Sumerian Mythology', 'Motion Graphics' ],
+				caption: 'Chapter 3'
 			}
 		},
-		{
-			type: 'S3',
-			media: {
-				source: 'public/humbaba/humbaba-making-of.mp4',
-				tags: [ 'Photogrammetry' ],
-				caption: 'Humbaba Caption'
-			}
-		},
+
 		{
 			type: 'S3',
 			media: {
 				source: 'public/humbaba/humbaba4.mp4',
-				tags: [ 'Korean Architecture' ],
-				caption: 'Humbaba Caption'
+				tags: [ 'Projection Mapping', 'Sumerian Mythology', 'Motion Graphics' ],
+				caption: 'Chapter 4'
 			}
 		},
 		{
 			type: 'S3',
 			media: {
 				source: 'public/humbaba/humbaba5.mp4',
-				tags: [ 'Photogrammetry' ],
-				caption: 'Humbaba Caption'
+				tags: [ 'Projection Mapping', 'Sumerian Mythology', 'Motion Graphics' ],
+				caption: 'Chapter 6'
 			}
 		},
+
+		{
+			type: 'S2',
+			title: 'Making-of',
+		},
+
+		{
+			type: 'S3',
+			anchor: 'Making-of',
+			media: {
+				source: 'public/humbaba/img/drawing.png',
+				tags: [ 'Projection Mapping', 'Sumerian Mythology', 'Motion Graphics' ],
+				caption: 'Initial Illustrator drawing'
+			}
+		},
+
 		{
 			type: 'S3',
 			media: {
 				excludeFromSphere: true,
 				source: 'public/humbaba/img/code04.jpg',
-				tags: [ 'Artificial Intelligence' ],
-				caption: 'Humbaba Caption'
+				tags: [ 'Projection Mapping', 'Sumerian Mythology', 'Motion Graphics' ],
+				caption: 'Disco Materials'
 			}
 		},
+
 		{
 			type: 'S3',
 			media: {
 				excludeFromSphere: true,
-				source: 'public/humbaba/img/code05.jpg',
-				tags: [ 'Photogrammetry' ],
-				caption: 'Humbaba Caption'
+				source: 'public/humbaba/img/code06.jpg',
+				tags: [ 'Projection Mapping', 'Sumerian Mythology', 'Motion Graphics' ],
+				caption: 'Material randomizing Xpresso graph prepared in Cinema 4D'
 			}
 		},
+
 		{
 			type: 'S3',
-			anchor: 'Anchor 3',
+			media: {
+				source: 'public/humbaba/humbaba-making-of.mp4',
+				tags: [ 'Projection Mapping', 'Sumerian Mythology', 'Motion Graphics' ],
+				caption: 'Building the Disco graph'
+			}
+		},
+
+		{
+			type: 'S3',
 			media: {
 				excludeFromSphere: true,
 				source: 'public/humbaba/img/code01.jpg',
-				tags: [ 'Artificial Intelligence' ],
-				caption: 'Humbaba Caption'
+				tags: [ 'Projection Mapping', 'Sumerian Mythology', 'Motion Graphics' ],
+				caption: 'Material randomizing Xpresso graph prepared in Cinema 4D'
 			}
 		},
 		{
@@ -124,80 +212,55 @@ export default {
 			media: {
 				excludeFromSphere: true,
 				source: 'public/humbaba/img/code02.jpg',
-				tags: [ 'Photogrammetry' ],
-				caption: 'Humbaba Caption'
+				tags: [ 'Projection Mapping', 'Sumerian Mythology', 'Motion Graphics' ],
+				caption: 'Graph Detail'
 			}
 		},
+
+
 		{
 			type: 'S3',
 			media: {
 				excludeFromSphere: true,
-				source: 'public/humbaba/img/code06.png',
-				tags: [ 'Photogrammetry' ],
-				caption: 'Humbaba Caption'
+
+				source: 'public/humbaba/img/edge.jpg',
+				tags: [ 'Projection Mapping', 'Sumerian Mythology', 'Motion Graphics' ],
+				caption: 'Line Animation in Cinema 4D'
 			}
 		},
+
 		{
 			type: 'S3',
 			media: {
-				source: 'public/humbaba/img/edge.png',
-				tags: [ 'Photogrammetry' ],
-				caption: 'Humbaba Caption'
-			}
-		},
-		{
-			type: 'S3',
-			media: {
-				source: 'public/humbaba/img/drawing.png',
-				tags: [ 'Photogrammetry' ],
-				caption: 'Humbaba Caption'
-			}
-		},
-		{
-			type: 'S3',
-			media: {
+				excludeFromSphere: true,
+
 				source: 'public/humbaba/img/rhino.png',
-				tags: [ 'Photogrammetry' ],
-				caption: 'Humbaba Caption'
+				tags: [ 'Projection Mapping', 'Sumerian Mythology', 'Motion Graphics' ],
+				caption: 'Pieces of the unfolded 3D model for building the cardboard model as projection base '
 			}
 		},
+
 		{
 			type: 'S3',
 			media: {
-				source: 'public/humbaba/img/test1.png',
-				tags: [ 'Photogrammetry' ],
-				caption: 'Humbaba Caption'
+				excludeFromSphere: true,
+
+				source: 'public/humbaba/img/madmapper1.png',
+				tags: [ 'Projection Mapping', 'Sumerian Mythology', 'Motion Graphics' ],
+				caption: 'Projection mapping'
 			}
 		},
+
 		{
 			type: 'S3',
 			media: {
-				source: 'public/humbaba/img/test2.png',
-				tags: [ 'Photogrammetry' ],
+
+				source: 'public/humbaba/humbaba-histogram.mp4',
+				tags: [ 'Projection Mapping', 'Sumerian Mythology', 'Motion Graphics' ],
 				caption: 'Humbaba Caption'
 			}
 		},
-		{
-			type: 'S3',
-			media: {
-				source: 'public/humbaba/img/test3.png',
-				tags: [ 'Photogrammetry' ],
-				caption: 'Humbaba Caption'
-			}
-		},
-		{
-			type: 'S3',
-			media: {
-				source: 'public/humbaba/img/test4.png',
-				tags: [ 'Photogrammetry' ],
-				caption: 'Humbaba Caption'
-			}
-		},
-		{
-			type: 'S2',
-			title: '3D Modelling<br>of Miniatures',
-			paragraphs: 'In this experiment, I made a 3D model of an Ottoman miniature representing the city of Constantinople. This virtual three-dimensional miniature allows us to go through the golden frame and the pictorial surface of pigments, and finally dive into the city from different angles. We can walk through its alleys, fly around its domes and explore the hidden corners of Constantinople as imagined by the miniaturist.'
-		}
+
 	]
 
 };
