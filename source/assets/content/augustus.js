bbcea4ef963b75c3d29abe03d87cc3ac5020935d
mete kutlu <metekutlu@gmail.com>
2022-12-31 02:05:24 +0100

    Update Augustus

--- source/assets/content/augustus.js
+++ source/assets/content/augustus.js
@@ -5,15 +5,44 @@ export default {
 	subtitle: 'Augmented Promenade in Medieval Paris',
 	location: 'Paris',
 	date: 'December 2022',
-	team: 'Mete Kutlu',
+	team: 'Mete Kutlu & Theuns Botha',
 	period: 'Ongoing',
 	description: 'In this experiment, I made a 3D model of an Ottoman miniature representing the city of Constantinople. This virtual three-dimensional miniature allows us to go through the golden frame and the pictorial surface of pigments, and finally dive into the city from different angles. We can walk through its alleys, fly around its domes and explore the hidden corners of Constantinople as imagined by the miniaturist.',
 	sections: [
 		{
 			type: 'S1',
 			media: {
-				source: 'public/augustus/intro.mp4',
-				caption: 'Analysis of the Tower of Adrianople',
+				source: 'public/augustus/motion-tracking.mp4',
+				caption: 'Video Introduction of the Augustus AR App',
+				tags: [ 'Augmented Reality', 'Architecture History', 'Medieval Architecture' ]
+			}
+		},
+		{
+			type: 'S2',
+			anchor: 'Introduction',
+			title: 'Introduction',
+		},
+		{
+			type: 'S3',
+			media: {
+				source: 'public/augustus/mockup01.mp4',
+				caption: 'Life-size virtual reconstruction of the Montgomery Tower seen in augmented reality',
+				tags: [ 'Augmented Reality', 'Architecture History', 'Medieval Architecture' ]
+			}
+		},
+		{
+			type: 'S3',
+			media: {
+				source: 'public/augustus/mockup-home.mp4',
+				caption: 'Augustus Homepage',
+				tags: [ 'Augmented Reality', 'Architecture History', 'Medieval Architecture' ]
+			}
+		},
+		{
+			type: 'S3',
+			media: {
+				source: 'public/augustus/cristiana.mp4',
+				caption: 'Tour Montgomery displayed in augmented reality on an iPad',
 				tags: [ 'Augmented Reality', 'Architecture History', 'Medieval Architecture' ]
 			}
 		},
@@ -21,7 +50,20 @@ export default {
 			type: 'S3',
 			media: {
 				source: 'public/augustus/experience.png',
-				caption: 'Importing assets inside an AR view',
+				caption: 'Tour Montgomery in AR',
+				tags: [ 'Augmented Reality', 'Architecture History', 'Medieval Architecture' ]
+			}
+		},
+		{
+			type: 'S2',
+			anchor: 'Storyboard',
+			title: 'Storyboard',
+		},
+		{
+			type: 'S3',
+			media: {
+				source: 'public/augustus/gps.png',
+				caption: 'GPS-based proximity notifications',
 				tags: [ 'Augmented Reality', 'Architecture History', 'Medieval Architecture' ]
 			}
 		},
@@ -29,15 +71,7 @@ export default {
 			type: 'S3',
 			media: {
 				source: 'public/augustus/stops01.jpg',
-				caption: 'Paris by Jean Fouquet, illumination from the manuscript of the Hours of Étienne Chevalier, around 1452, Metropolitan Museum of New York, 1975.1.2490',
-				tags: [ 'Augmented Reality', 'Architecture History', 'Medieval Architecture' ]
-			}
-		},
-		{
-			type: 'S3',
-			media: {
-				source: 'public/augustus/stops02.jpg',
-				caption: 'Paris by Jean Fouquet, illumination from the manuscript of the Hours of Étienne Chevalier, around 1452, Metropolitan Museum of New York, 1975.1.2490',
+				caption: 'Stop 1: Rue Clovis',
 				tags: [ 'Augmented Reality', 'Architecture History', 'Medieval Architecture' ]
 			}
 		},
@@ -45,7 +79,7 @@ export default {
 			type: 'S3',
 			media: {
 				source: 'public/augustus/stops03.jpg',
-				caption: 'Paris by Jean Fouquet, illumination from the manuscript of the Hours of Étienne Chevalier, around 1452, Metropolitan Museum of New York, 1975.1.2490',
+				caption: 'Stop 3: Firestation',
 				tags: [ 'Augmented Reality', 'Architecture History', 'Medieval Architecture' ]
 			}
 		},
@@ -53,7 +87,15 @@ export default {
 			type: 'S3',
 			media: {
 				source: 'public/augustus/stops04.jpg',
-				caption: 'Paris by Jean Fouquet, illumination from the manuscript of the Hours of Étienne Chevalier, around 1452, Metropolitan Museum of New York, 1975.1.2490',
+				caption: 'Stop 4: La Tournelle',
+				tags: [ 'Augmented Reality', 'Architecture History', 'Medieval Architecture' ]
+			}
+		},
+		{
+			type: 'S3',
+			media: {
+				source: 'public/augustus/stops02.jpg',
+				caption: 'Stop 5: Montgomery Tower',
 				tags: [ 'Augmented Reality', 'Architecture History', 'Medieval Architecture' ]
 			}
 		},
@@ -61,21 +103,14 @@ export default {
 			type: 'S3',
 			media: {
 				source: 'public/augustus/stops05.jpg',
-				caption: 'Paris by Jean Fouquet, illumination from the manuscript of the Hours of Étienne Chevalier, around 1452, Metropolitan Museum of New York, 1975.1.2490',
-				tags: [ 'Augmented Reality', 'Architecture History', 'Medieval Architecture' ]
-			}
-		},
-		{
-			type: 'S3',
-			media: {
-				source: 'public/augustus/gps.png',
-				caption: 'Importing assets inside an AR view',
+				caption: 'Stop 5: Montgomery Tower',
 				tags: [ 'Augmented Reality', 'Architecture History', 'Medieval Architecture' ]
 			}
 		},
 		{
 			type: 'S2',
 			title: 'Iconographic Research',
+			anchor: 'Iconographic Research',
 		},
 		{
 			type: 'S3',
@@ -103,19 +138,27 @@ export default {
 		},
 		{
 			type: 'S2',
-			title: 'Making-of',
+			title: 'Workflow',
+			anchor: 'Workflow',
 		},
 		{
 			type: 'S3',
 			media: {
+				excludeFromSphere: true,
 				source: 'public/augustus/workflow.png',
-				caption: 'Importing assets inside an AR view',
+				caption: 'Workflow',
 				tags: [ 'Augmented Reality', 'Architecture History', 'Medieval Architecture' ]
 			}
 		},
 		{
+			type: 'S2',
+			title: 'Developing with ARKit',
+			anchor: 'Developing with ARKit',
+		},
+		{
 			type: 'S3',
 			media: {
+				excludeFromSphere: true,
 				source: 'public/augustus/code01.png',
 				caption: 'Importing assets inside an AR view',
 				tags: [ 'Augmented Reality', 'Architecture History', 'Medieval Architecture' ]
@@ -124,6 +167,7 @@ export default {
 		{
 			type: 'S3',
 			media: {
+				excludeFromSphere: true,
 				source: 'public/augustus/code01.png',
 				caption: 'Importing assets inside an AR view',
 				tags: [ 'Augmented Reality', 'Architecture History', 'Medieval Architecture' ]
@@ -132,6 +176,7 @@ export default {
 		{
 			type: 'S3',
 			media: {
+				excludeFromSphere: true,
 				source: 'public/augustus/code02.png',
 				caption: 'Anchoring 3D models to 3D object targets using ARKit Scanner',
 				tags: [ 'Augmented Reality', 'Architecture History', 'Medieval Architecture' ]
@@ -140,14 +185,21 @@ export default {
 		{
 			type: 'S3',
 			media: {
-				source: 'public/augustus/scene.png',
-				caption: 'Scene setup',
+				excludeFromSphere: true,
+				source: 'public/augustus/ar-scanner.png',
+				caption: 'Creation of 3D anchors triggering AR experiences',
 				tags: [ 'Augmented Reality', 'Architecture History', 'Medieval Architecture' ]
 			}
 		},
 		{
+			type: 'S2',
+			title: '3D Models',
+			anchor: '3D Models',
+		},
+		{
 			type: 'S3',
 			media: {
+				excludeFromSphere: true,
 				source: 'public/augustus/uvmap.png',
 				caption: 'UV mapping of 3D models',
 				tags: [ 'Augmented Reality', 'Architecture History', 'Medieval Architecture' ]
@@ -156,14 +208,30 @@ export default {
 		{
 			type: 'S3',
 			media: {
-				source: 'public/augustus/interaction.png',
-				caption: 'Integrating interactive properties to scenes with Reality Composer',
+				excludeFromSphere: true,
+				source: 'public/augustus/scene.png',
+				caption: 'Scene setup',
 				tags: [ 'Augmented Reality', 'Architecture History', 'Medieval Architecture' ]
 			}
 		},
 		{
 			type: 'S3',
 			media: {
+				excludeFromSphere: true,
+				source: 'public/augustus/interaction.png',
+				caption: 'Integrating interactive properties to scenes with Reality Composer',
+				tags: [ 'Augmented Reality', 'Architecture History', 'Medieval Architecture' ]
+			}
+		},
+		{
+			type: 'S2',
+			title: 'Survey in AR',
+			anchor: 'Survey in AR',
+		},
+		{
+			type: 'S3',
+			media: {
+				excludeFromSphere: true,
 				source: 'public/augustus/make01.png',
 				caption: 'Importing assets inside an AR view',
 				tags: [ 'Augmented Reality', 'Architecture History', 'Medieval Architecture' ]
@@ -172,6 +240,7 @@ export default {
 		{
 			type: 'S3',
 			media: {
+				excludeFromSphere: true,
 				source: 'public/augustus/make02.png',
 				caption: 'Importing assets inside an AR view',
 				tags: [ 'Augmented Reality', 'Architecture History', 'Medieval Architecture' ]
@@ -180,8 +249,46 @@ export default {
 		{
 			type: 'S3',
 			media: {
+				excludeFromSphere: true,
 				source: 'public/augustus/make03.png',
 				caption: 'Importing assets inside an AR view',
+				tags: [ 'Augmented Reality', 'Architecture History', 'Medieval Architecture' ]
+			}
+		},
+		{
+			type: 'S2',
+			title: 'Motion Tracking & Compositing',
+			anchor: 'Motion Tracking & Compositing',
+		},
+		{
+			type: 'S3',
+			media: {
+				excludeFromSphere: true,
+				source: 'public/augustus/tracking01.png',
+				caption: 'Importing assets inside an AR view',
+				tags: [ 'Augmented Reality', 'Architecture History', 'Medieval Architecture' ]
+			}
+		},
+		{
+			type: 'S3',
+			media: {
+				excludeFromSphere: true,
+				source: 'public/augustus/tracking02.png',
+				caption: 'Importing assets inside an AR view',
+				tags: [ 'Augmented Reality', 'Architecture History', 'Medieval Architecture' ]
+			}
+		},
+		{
+			type: 'S2',
+			title: 'SDK Testing',
+			anchor: 'SDK Testing',
+		},
+		{
+			type: 'S3',
+			media: {
+				excludeFromSphere: true,
+				source: 'public/augustus/test-unity.mp4',
+				caption: 'Initial testing of different SDKs',
 				tags: [ 'Augmented Reality', 'Architecture History', 'Medieval Architecture' ]
 			}
 		},
