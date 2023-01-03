a5a9f897b041255c7d21ed00f05780df90091f4f
mete kutlu <metekutlu@gmail.com>
2023-01-03 03:20:09 +0100

    Firuze Update

---
+++ source/assets/content/firuze.js
@@ -0,0 +1,53 @@
+export default {
+
+	path: 'firuze',
+	title: 'Firuze',
+	subtitle: 'Digitizing thr Timurid Mosaics',
+	location: 'Paris',
+	date: 'October 2022',
+	team: 'Mete Kutlu',
+	period: 'October 2022',
+	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam volutpat felis vitae lorem scelerisque placerat. Quisque est dolor, euismod ac ornare vulputate, gravida ac nunc. Nunc suscipit lacinia ante non hendrerit. Donec porta ut leo eu suscipit. Nullam vitae sagittis sapien. Sed lacinia tincidunt risus non scelerisque. Pellentesque et nulla porttitor eros pharetra semper. Nam id dui nec purus condimentum pulvinar. Vivamus a tortor arcu. Ut eu lorem eu turpis vulputate gravida. Duis ut viverra risus. Quisque volutpat in enim in consequat. Mauris vitae efficitur urna, nec interdum erat. ',
+	sections: [
+		{
+			type: 'S1',
+			media: {
+				source: 'public/firuze/firuze-intro.mp4',
+				caption: 'Firuze Video Introduction',
+				tags: [ 'Timurid Architecture', 'Architectural Heritage', 'Photogrammetry' ]
+			}
+		},
+		{
+			type: 'S3',
+			media: {
+				source: 'public/firuze/tower01.mp4',
+				caption: 'Firuze Video Introduction',
+				tags: [ 'Timurid Architecture', 'Architectural Heritage', 'Photogrammetry' ]
+			}
+		},
+		{
+			type: 'S3',
+			media: {
+				source: 'public/firuze/city01.mp4',
+				caption: 'Firuze Video Introduction',
+				tags: [ 'Timurid Architecture', 'Architectural Heritage', 'Photogrammetry' ]
+			}
+		},
+		{
+			type: 'S3',
+			media: {
+				source: 'public/firuze/castle01.mp4',
+				caption: 'Firuze Video Introduction',
+				tags: [ 'Timurid Architecture', 'Architectural Heritage', 'Photogrammetry' ]
+			},
+		},
+		{
+			type: 'S3',
+			media: {
+				source: 'public/firuze/wall02.png',
+				caption: 'Firuze Video Introduction',
+				tags: [ 'Timurid Architecture', 'Architectural Heritage', 'Photogrammetry' ]
+			},
+		},
+	]
+};
