be97cfc2b69b9abb35a3ba87a5b3eb49ea312219
metekutlu07 <81023690+metekutlu07@users.noreply.github.com>
2026-04-03 03:35:19 +0200

    tessera

--- source/assets/content/tessera.js
+++ source/assets/content/tessera.js
@@ -12,64 +12,83 @@ export default {
 		{
 			type: 'S1',
 			media: {
-				source: 'public/tessera/main.jpg',
-				caption: 'Video Presentation',
+				source: 'public/tessera/mermaid.jpg',
+				caption: '',
 				tags: [ 'Augmented Reality', 'Roman', 'Mosaics' ],
 			}
 		},
 		{
-			type: 'S9',
-			media: {
-				source: 'public/tessera/main.jpg',
-				caption: 'Homepage',
-				tags: [ 'Augmented Reality', 'Roman', 'Mosaics' ],
-			}
+			type: 'S2',
+			paragraphs: 'Tessera is an augmented reality prototype conceived as a digital mediation tool for Roman mosaics. The project forms part of a broader investigation into an alternative archaeology of the pixel, which examines mosaics as early image systems based on the aggregation of discrete units. Mosaics are reconsidered as artisanal simulation media, capable of producing immersive spatial experiences that can be understood as proto-augmented reality. The project was initiated during a one-month workshop titled <em>From Mosaics to Pixels</em>, organized by the UNESCO Club Volubilis in Avignon, with the support of the Regional Municipality of Provence–Alpes–Côte d’Azur and in partnership with the municipality of Carthage and the Tunisian Ministry of Culture. This workshop marked a turning point in the development of my research, as I became directly engaged with digital scenography, heritage reconstruction, innovation in cultural mediation, and programming. It is based on the reconstruction and repositioning of mosaic panels within their original archaeological context in the Villas Quarter of Carthage.',
 		},
 		{
-			type: 'S9',
-			media: {
-				source: 'public/tessera/echelle.jpg',
-				caption: 'Game levels and the scales of experience',
-				tags: [ 'Augmented Reality', 'Roman', 'Mosaics' ],
-			}
+			type: 'S12',
+			groups: [
+				{
+					title: 'Sponsor',
+					lines: [ 'Regional Municipality of Provence–Alpes–Côte d’Azur' ]
+				},
+				{
+					title: 'Partners',
+					lines: [ 'Municipality of Carthage', 'National Heritage Institute of Tunisia (INP)', 'Heritage Development and Cultural Promotion Agency (AMVPPC)', 'Friends of Carthage Association' ]
+				},
+				{
+					title: 'Organizer',
+					lines: [ 'UNESCO Club Volubilis (Avignon)' ]
+				}
+			],
 		},
 		{
-			type: 'S9',
-			media: {
-				source: 'public/tessera/carthage.jpg',
-				caption: 'Creating different itineraries (City Level)',
-				tags: [ 'Augmented Reality', 'Roman', 'Mosaics' ],
-			}
-		},
-		{
-			type: 'S9',
-			media: {
-				source: 'public/tessera/parc.jpg',
-				caption: 'Tessera Collection and levelling up (Architectural Level)',
-				tags: [ 'Augmented Reality', 'Roman', 'Mosaics' ],
-			}
-		},
-		{
-			type: 'S9',
-			media: {
-				source: 'public/tessera/maison.jpg',
-				caption: 'Mare Nostrum (Mediterranean Level)',
-				tags: [ 'Augmented Reality', 'Roman', 'Mosaics' ],
-			}
-		},
-		{
-			type: 'S9',
-			media: {
-				source: 'public/tessera/map.jpg',
-				caption: 'Visualization of mosaic floors in augmented reality',
-				tags: [ 'Augmented Reality', 'Roman', 'Mosaics' ],
-			}
-		},
-		{
-			type: 'S9',
+			type: 'S3',
 			media: {
 				source: 'public/tessera/mosaic-ar.jpg',
-				caption: 'Completition of original mosaic panels with projected frescoes',
+				caption: 'Augmented reality reconstruction of the Aviary House mosaics.',
+				explain: 'Concept drawing for Mosaic Go! AR app. Drawing by the author, June 2019.',
+				tags: [ 'Augmented Reality', 'Roman', 'Mosaics' ],
+			}
+		},
+		{
+			type: 'S3',
+			media: {
+				source: 'public/tessera/maiso1.jpg',
+				caption: 'Ruins of the atrium of the Aviary House.',
+				explain: 'With an octagonal viridarium surrounded by a peristyle colonnade. <em>The Mosaic of the Strewn Plants</em> once adorned this residential district on the Odeon Hill, Roman period, Carthage (Tunisia), 4th century. Photograph by the author, June 2019.',
+				tags: [ 'Augmented Reality', 'Roman', 'Mosaics' ],
+			}
+		},
+		{
+			type: 'S3',
+			media: {
+				source: 'public/tessera/maiso2.jpg',
+				caption: 'The atrium seen from the galleries and the copied mosaic fragments on the floor.',
+				explain: 'At the center is an octagonal viridarium, surrounded by a peristyle colonnade. <em>The Mosaic of the Strewn Plants</em> originally decorated the Roman villa district on the Odeon Hill. Roman period, Carthage (Tunisia), 4th century. Photograph by the author, June 2019.',
+				tags: [ 'Augmented Reality', 'Roman', 'Mosaics' ],
+			}
+		},
+		{
+			type: 'S3',
+			media: {
+				source: 'public/tessera/maiso3.jpg',
+				caption: 'Franco-Tunisian project team, Carthage (Tunisia), June 2019. ',
+				explain: 'From Mosaics to Pixels, a workshop organized by the UNESCO Club Volubilis. Photograph from the author’s collection.',
+				tags: [ 'Augmented Reality', 'Roman', 'Mosaics' ],
+			}
+		},
+		{
+			type: 'S3',
+			media: {
+				source: 'public/tessera/maiso4.jpg',
+				caption: 'The snail on the left edge of the mosaic fragment. ',
+				explain: 'Copied paels arranged around the atrium, <em>The Mosaic of the Strewn Plants,</em> Roman period, Carthage, 4th century. Photograph by the author, June 2019.',
+				tags: [ 'Augmented Reality', 'Roman', 'Mosaics' ],
+			}
+		},
+		{
+			type: 'S8',
+			media: {
+				source: 'public/tessera/maiso5.jpg',
+				caption: 'Mosaic of the Strewn Plants, House of the Aviary, Roman Period, Carthage (Tunisia), late 3rd or early 4th century.',
+				explain: 'The mosaics decorating the floor of the atrium around the octagonal viridarium. The mosaic floor is removed from its  original place for preservation. Its fragments are reassembled to form  a rectangular mosaic carpet, currently exposed at the Bardo Museum in Tunis. Image from <em>Splendeurs des mosaïques de Tunisie,</em> Mohamed Yacoub, Ministère de la Culture, de la Jeunesse et des Loisirs, 2002.',
 				tags: [ 'Augmented Reality', 'Roman', 'Mosaics' ],
 			}
 		},
@@ -77,15 +96,63 @@ export default {
 			type: 'S3',
 			media: {
 				source: 'public/tessera/triclin01.jpg',
-				caption: 'Presentation Video',
+				caption: 'Immersive triclinium, Mete Kutlu, 2019.',
+				explain: 'The xenia mosaic panel is repositioned in its original location within the triclinium, the dining room of the House of the Aviary. The frescoes are reconstructed through projection, based on surviving examples such as those in Pompeii.',
+				tags: [ 'Augmented Reality', 'Roman', 'Mosaics' ],
+			}
+		},
+		{
+			type: 'S3',
+			media: {
+				source: 'public/tessera/triclin02.jpg',
+				caption: 'Video presentation - resting area during visit, Mete Kutlu, 2019.',
+				tags: [ 'Augmented Reality', 'Roman', 'Mosaics' ],
+			}
+		},
+		{
+			type: 'S2',
+			paragraphs: 'The prototype allows users to visualize life-size virtual reconstructions of mosaics in situ. Many of these panels are now preserved in the Bardo Museum, leaving the archaeological sites without their original surfaces. The system restores these “stone carpets” to their initial locations, enabling visitors to perceive the sites as integrated visual and spatial environments. The interface is designed to guide attention toward the surrounding landscape, the ruins, and their historical context, rather than focusing on the screen itself. It operates through a game-like structure in which the visit is framed as a journey through the Roman period. It encourages movement across multiple sites and supports the discovery of a broader Mediterranean network. The experience extends from the scale of the monument to that of the city and the sea, forming a distributed system of connected sites. In this perspective, each institution becomes a tessera, forming a vast institutional mosaic that encourages international collaboration through shared history.',
+		},
+		{
+			type: 'S9',
+			media: {
+				source: 'public/tessera/echelle.jpg',
+				caption: 'Game levels and the scales of experience, Mete Kutlu, 2019.',
 				tags: [ 'Augmented Reality', 'Roman', 'Mosaics' ],
 			}
 		},
 		{
 			type: 'S9',
 			media: {
-				source: 'public/tessera/triclin02.jpg',
-				caption: 'Interactive ',
+				source: 'public/tessera/carthage.jpg',
+				caption: 'Creating different itineraries (City Level).',
+				explain: 'Plan of Carthage, enabling exploration of the city across its Punic, Roman, Vandal, and Byzantine layers. Mosaic Go! AR app. Drawing by the author, June 2019.',
+				tags: [ 'Augmented Reality', 'Roman', 'Mosaics' ],
+			}
+		},
+		{
+			type: 'S9',
+			media: {
+				source: 'public/tessera/parc.jpg',
+				caption: 'Game Level I: District of Roman Villas.',
+				explain: 'The player collects tesserae by exploring monuments and ruins in the Roman Villas district. Mosaic Go! AR app. Drawing by the author, June 2019.',
+				tags: [ 'Augmented Reality', 'Roman', 'Mosaics' ],
+			}
+		},
+		{
+			type: 'S9',
+			media: {
+				source: 'public/tessera/maison.jpg',
+				caption: 'Game Level II: The Aviary House.',
+				explain: 'The player collects tesserae by exploring the mosaic floors of the house in the Roman Villas district. Mosaic Go! AR app. Drawing by the author, June 2019.',
+				tags: [ 'Augmented Reality', 'Roman', 'Mosaics' ],
+			}
+		},
+		{
+			type: 'S9',
+			media: {
+				source: 'public/tessera/map.jpg',
+				caption: 'A cultural game across the Mediterranean, Mete Kutlu, 2019.',
 				tags: [ 'Augmented Reality', 'Roman', 'Mosaics' ],
 			}
 		},
@@ -93,7 +160,7 @@ export default {
 			type: 'S9',
 			media: {
 				source: 'public/tessera/cheveaux.jpg',
-				caption: 'Video Presentation',
+				caption: 'Information related to mosaic panels in proximity, Mete Kutlu, 2019.',
 				tags: [ 'Augmented Reality', 'Roman', 'Mosaics' ],
 			}
 		},
@@ -101,7 +168,7 @@ export default {
 			type: 'S9',
 			media: {
 				source: 'public/tessera/menu.jpg',
-				caption: 'Video Presentation',
+				caption: 'Statistics for the institutions, Mete Kutlu, 2019.',
 				tags: [ 'Augmented Reality', 'Roman', 'Mosaics' ],
 			}
 		},
